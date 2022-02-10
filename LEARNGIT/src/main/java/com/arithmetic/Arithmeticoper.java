8a96d1e89991d0a2a94e8f8c0cb1abfa4b0c555f
AATHIRARAJU <86960005+AATHIRARAJU@users.noreply.github.com>
2022-02-10 18:19:18 +0530

    Add files via upload

---
+++ LEARNGIT/src/main/java/com/arithmetic/Arithmeticoper.java
@@ -0,0 +1,18 @@
+package com.arithmetic;
+
+public class Arithmeticoper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int a=5;
+		int b=3;
+		int c;
+		
+		c=a+b;
+		
+System.out.println("sum is ="+c);
+	}
+
+}
